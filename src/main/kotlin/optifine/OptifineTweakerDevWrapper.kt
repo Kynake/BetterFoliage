530124c5936e8c6eee0126a2896a8e2b1cc18980
Kynake Risk <kynakerisk@gmail.com>
2025-10-12 19:07:02 -0300

    Suppress optifine dev warnings

--- src/main/kotlin/optifine/OptifineTweakerDevWrapper.kt
+++ src/main/kotlin/optifine/OptifineTweakerDevWrapper.kt
@@ -1,13 +1,14 @@
 package optifine
 
+import java.io.File
 import net.minecraft.launchwrapper.IClassTransformer
 import net.minecraft.launchwrapper.ITweaker
 import net.minecraft.launchwrapper.LaunchClassLoader
-import java.io.File
 
+@Suppress("unused")
 class OptifineTweakerDevWrapper : ITweaker {
     override fun acceptOptions(p0: MutableList<String>?, p1: File?, p2: File?, p3: String?) {}
-    override fun getLaunchArguments(): Array<out String>? = Array<String>(0) { "" }
+    override fun getLaunchArguments(): Array<out String> = Array(0) { "" }
     override fun getLaunchTarget() = "net.minecraft.client.main.Main"
     override fun injectIntoClassLoader(classLoader: LaunchClassLoader) {
         classLoader.registerTransformer("optifine.OptifineTransformerDevWrapper")
@@ -20,8 +21,10 @@ class OptifineTweakerDevWrapper : ITweaker {
  * This class is only used in development to debug cross-mod issues with Optifine, and is not part
  * of the release!
  */
+@Suppress("unused")
 class OptifineTransformerDevWrapper : IClassTransformer {
 
+    @Suppress("DEPRECATION")
     val ofTransformer =
         Class.forName("optifine.OptiFineClassTransformer").newInstance() as IClassTransformer
 
@@ -29,5 +32,5 @@ class OptifineTransformerDevWrapper : IClassTransformer {
      * Call the Optifine transformer, but change dots to slashes in class names. This enables the
      * Optifine transformer to load replacements from non-root locations in the jar file.
      */
-    override fun transform(name: String?, transformedName: String?, classData: ByteArray?) = ofTransformer.transform(name?.replace(".", "/"), transformedName, classData)
+    override fun transform(name: String?, transformedName: String?, classData: ByteArray?): ByteArray = ofTransformer.transform(name?.replace(".", "/"), transformedName, classData)
 }
