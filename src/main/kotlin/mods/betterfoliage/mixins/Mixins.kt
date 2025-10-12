716599f3b8d1fe1274e06b574c98dac1acb1a9f0
Kynake Risk <kynakerisk@gmail.com>
2025-10-12 15:13:31 -0300

    Enable mixins skeleton

---
+++ src/main/kotlin/mods/betterfoliage/mixins/Mixins.kt
@@ -0,0 +1,7 @@
+﻿package mods.betterfoliage.mixins
+
+import com.gtnewhorizon.gtnhmixins.builders.IMixins
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder
+
+enum class Mixins : IMixins {
+}
