36593d6517eed540ce1dc2a2f1e7c972778430e5
Kynake Risk <kynakerisk@gmail.com>
2025-10-26 16:34:28 -0300

    Enable support for late mixins

---
+++ src/main/java/mods/betterfoliage/mixins/LateMixinsLoader.java
@@ -0,0 +1,24 @@
+package mods.betterfoliage.mixins;
+
+import java.util.List;
+import java.util.Set;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+
+@LateMixin
+public class LateMixinsLoader implements ILateMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.BetterFoliage.late.json";
+    }
+
+    @Override
+    public @NotNull List<String> getMixins(Set<String> loadedMods) {
+        return IMixins.getLateMixins(Mixins.class, loadedMods);
+    }
+}
