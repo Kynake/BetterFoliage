c304490750f36878563e8426b2eb71e7c10f3573
Kynake Risk <kynakerisk@gmail.com>
2026-01-30 11:01:58 -0300

    Update buildscript and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,6 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gtnewhorizons.gtnhsettingsconvention") version("1.0.48")
+    id("com.gtnewhorizons.gtnhsettingsconvention") version("1.0.49")
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
