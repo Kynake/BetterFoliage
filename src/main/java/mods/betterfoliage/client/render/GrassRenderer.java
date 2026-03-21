3e244c24673b73081309e429d67a352ed4432404
Kynake Risk <kynakerisk@gmail.com>
2026-03-21 01:46:56 -0300

    Create first develop version of new Grass Renderer

---
+++ src/main/java/mods/betterfoliage/client/render/GrassRenderer.java
@@ -0,0 +1,42 @@
+package mods.betterfoliage.client.render;
+
+import mods.betterfoliage.BetterFoliageMod;
+import mods.betterfoliage.client.config.Config;
+import mods.octarinecore.client.render.BlockContext;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class GrassRenderer extends BlockRenderer {
+
+    private static GrassRenderer instance;
+
+    public static GrassRenderer getInstance() {
+        if(instance == null) {
+            BetterFoliageMod.log.info("Registering NEW! GrassRenderer");
+            instance = new GrassRenderer();
+        }
+
+        return instance;
+    }
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
+        IIcon grassTop = block.getIcon(world, x, y, z, ForgeDirection.UP.ordinal());
+
+        renderer.setOverrideBlockTexture(grassTop);
+        boolean renderResult = renderer.renderStandardBlock(block, x, y, z);
+        renderer.clearOverrideBlockTexture();
+        return renderResult;
+    }
+
+    @Override
+    public boolean isEligible(BlockContext ctx) {
+        return Config.INSTANCE.getEnabled() &&
+            ctx.getCameraDistance() < Config.shortGrass.INSTANCE.getDistance() &&
+            (Config.shortGrass.INSTANCE.getGrassEnabled() || Config.connectedGrass.INSTANCE.getEnabled()) &&
+            Config.blocks.INSTANCE.getGrass().matchesID(ctx.getBlock());
+    }
+}
