7a6fe7902078b1b553d8838b8da47dcec4a799f1
Kynake Risk <kynakerisk@gmail.com>
2026-03-21 01:46:37 -0300

    Create new custom renderer base class

---
+++ src/main/java/mods/betterfoliage/client/render/BlockRenderer.java
@@ -0,0 +1,32 @@
+package mods.betterfoliage.client.render;
+
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import mods.octarinecore.client.render.BlockContext;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+
+public abstract class BlockRenderer implements ISimpleBlockRenderingHandler {
+    private final int renderId;
+
+    protected BlockRenderer() {
+        renderId = RenderingRegistry.getNextAvailableRenderId();
+        RenderingRegistry.registerBlockHandler(this);
+    }
+
+    @Override
+    public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {}
+
+    @Override
+    public boolean shouldRender3DInInventory(int modelId) {
+        return true;
+    }
+
+    @Override
+    public int getRenderId() {
+        return renderId;
+    }
+
+    public abstract boolean isEligible(BlockContext context);
+
+}
