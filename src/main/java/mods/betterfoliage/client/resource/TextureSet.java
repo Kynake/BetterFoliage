269ebbf439b7800af4e2e96f5975f6d3c1ce2643
Kynake Risk <kynakerisk@gmail.com>
2026-03-29 03:43:18 -0300

    Create TextureSet class to handle access to texture variations

---
+++ src/main/java/mods/betterfoliage/client/resource/TextureSet.java
@@ -0,0 +1,83 @@
+package mods.betterfoliage.client.resource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import mods.betterfoliage.BetterFoliageMod;
+import mods.betterfoliage.Utils;
+
+public class TextureSet {
+
+    private final String domain;
+    private final String prefix;
+    private final String suffix;
+
+    private List<IIcon> textures;
+    private long seed;
+
+    public TextureSet(String domain, String prefix, String suffix) {
+        this.domain = domain;
+        this.prefix = prefix;
+        this.suffix = suffix;
+
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance()
+            .bus()
+            .register(this);
+    }
+
+    @SubscribeEvent
+    public void onTextureStitch(TextureStitchEvent.Pre event) {
+        int type = event.map.getTextureType();
+        boolean isValid = (type == 0 && prefix.startsWith("textures/blocks/"))
+            || (type == 1 && prefix.startsWith("textures/items/"));
+
+        if (!isValid) {
+            return;
+        }
+
+        HashSet<ResourceLocation> resources = ResourceManager.findResourcesWithPattern(domain, prefix, suffix);
+        textures = new ArrayList<>(resources.size());
+
+        for (ResourceLocation res : resources) {
+            String name = res.getResourcePath();
+            int startIndex = name.lastIndexOf('/');
+            int endIndex = name.lastIndexOf('.');
+
+            if (startIndex < 0 || startIndex > endIndex) {
+                BetterFoliageMod.log.error("Invalid resource location: {}", res);
+                continue;
+            }
+
+            String textureName = domain + ":" + name.substring(startIndex + 1, endIndex);
+            textures.add(event.map.registerIcon(textureName));
+        }
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        seed = event.world.getWorldInfo()
+            .getSeed();
+    }
+
+    public IIcon getTextureForLocation(int x, int y, int z) {
+        if (textures == null || textures.isEmpty()) {
+            return null;
+        }
+
+        int index = Utils.hashWorldCoords(x, y, z, seed) % textures.size();
+        if (index < 0) {
+            index = -index;
+        }
+        return textures.get(index);
+    }
+}
