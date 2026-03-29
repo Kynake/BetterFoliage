8a8c6bbe7eaed2adb9c29d22d7199f9b19beabd5
Kynake Risk <kynakerisk@gmail.com>
2026-03-29 03:39:32 -0300

    Move ResourceHandler to resources package

--- src/main/java/mods/betterfoliage/client/resource/ResourceHandler.java
+++ src/main/java/mods/betterfoliage/client/resource/ResourceHandler.java
@@ -1,10 +1,12 @@
-package mods.betterfoliage.client.render;
+package mods.betterfoliage.client.resource;
 
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+// TODO: This class might not be needed
 
 // TODO: Add other SubscribeEvents that need to be handled (either here or somewhere else)
 public abstract class ResourceHandler {
