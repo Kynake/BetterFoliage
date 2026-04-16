7b089a09745fc471dc27f876d612668091fa8bb0
Kynake Risk <kynakerisk@gmail.com>
2026-04-16 01:51:36 -0300

    Implement generated short grass texture from the top 6 pixels of the tall grass texture

--- src/main/java/mods/betterfoliage/client/resource/generators/ShortGrassGenerator.java
+++ src/main/java/mods/betterfoliage/client/resource/generators/ShortGrassGenerator.java
@@ -1,5 +1,6 @@
 package mods.betterfoliage.client.resource.generators;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -12,6 +13,10 @@ import net.minecraft.util.ResourceLocation;
 import mods.betterfoliage.client.resource.ResourceUtils;
 
 public class ShortGrassGenerator extends SingleTextureGenerator {
+
+    // TODO make configurable?
+    // Only use the top 6 / 16 pixels from the base texture
+    private static final float topCutRatio = 6.f / 16.f;
 
     public ShortGrassGenerator(String baseDomain, String baseResourcePath) {
         super("Generated Short Grass", "gen_grass", baseDomain, baseResourcePath);
@@ -31,8 +36,18 @@ public class ShortGrassGenerator extends SingleTextureGenerator {
             throw new IIOException("Base image for generation of resource " + location + " is null!");
         }
 
-        BufferedImage genImage = createCopy(baseImage);
-        debugPaintRed(genImage);
+        BufferedImage genImage = createEmptyFrom(baseImage);
+        Graphics2D graphics = genImage.createGraphics();
+
+        int width = baseImage.getWidth();
+        int frames = baseImage.getHeight() / width;
+        int topPixels = (int) (width * topCutRatio);
+
+        for (int frame = 0; frame < frames; frame++) {
+            BufferedImage baseSubFrame = baseImage.getSubimage(0, width * frame, width, topPixels);
+            graphics.drawImage(baseSubFrame, 0, width * (frame + 1) - topPixels, null);
+        }
+
         return genImage;
     }
 }
