fa9e870250fecc834871cc5f5dc75924654ab1db
Kynake Risk <kynakerisk@gmail.com>
2026-04-19 03:36:06 -0300

    remove references to old short grass models from algae and mycelium renderers

--- src/main/kotlin/mods/betterfoliage/client/render/RenderAlgae.kt
+++ src/main/kotlin/mods/betterfoliage/client/render/RenderAlgae.kt
@@ -4,21 +4,47 @@ import mods.betterfoliage.BetterFoliageMod
 import mods.betterfoliage.client.Client
 import mods.betterfoliage.client.config.Config
 import mods.octarinecore.client.render.AbstractBlockRenderingHandler
+import mods.octarinecore.client.render.Axis
 import mods.octarinecore.client.render.BlockContext
+import mods.octarinecore.client.render.Model
 import mods.octarinecore.client.render.Rotation
 import mods.octarinecore.client.render.alwaysRender
+import mods.octarinecore.client.render.cornerAo
+import mods.octarinecore.client.render.cornerFlat
+import mods.octarinecore.client.render.faceOrientedAuto
 import mods.octarinecore.client.render.modelRenderer
 import mods.octarinecore.client.render.noPost
+import mods.octarinecore.random
 import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.RenderBlocks
+import net.minecraftforge.common.util.ForgeDirection.UP
 import org.apache.logging.log4j.Level.INFO
 
 class RenderAlgae : AbstractBlockRenderingHandler(BetterFoliageMod.MOD_ID) {
 
+    // Based on the old short grass crossed square renderer
+    companion object {
+        @JvmStatic
+        val algaeTopQuads: Model.(Int) -> Unit = { modelIdx ->
+            verticalRectangle(
+                x1 = -0.5,
+                z1 = 0.5,
+                x2 = 0.5,
+                z2 = -0.5,
+                yBottom = 0.5,
+                yTop = 0.5 + random(Config.shortGrass.heightMin, Config.shortGrass.heightMax),
+            )
+                .setAoShader(faceOrientedAuto(overrideFace = UP, corner = cornerAo(Axis.Y)))
+                .setFlatShader(faceOrientedAuto(overrideFace = UP, corner = cornerFlat))
+                .toCross(UP) { it.move(xzDisk(modelIdx) * Config.shortGrass.hOffset) }
+                .addAll()
+        }
+    }
+
     val noise = simplexNoise()
 
     val algaeIcons = iconSet(BetterFoliageMod.LEGACY_DOMAIN, "better_algae_%d")
-    val algaeModels = modelSet(64, RenderGrass.grassTopQuads)
+    val algaeModels = modelSet(64, algaeTopQuads)
 
     override fun afterStitch() {
         Client.log(INFO, "Registered ${algaeIcons.num} algae textures")
--- src/main/kotlin/mods/betterfoliage/client/render/RenderMycelium.kt
+++ src/main/kotlin/mods/betterfoliage/client/render/RenderMycelium.kt
@@ -4,21 +4,47 @@ import mods.betterfoliage.BetterFoliageMod
 import mods.betterfoliage.client.Client
 import mods.betterfoliage.client.config.Config
 import mods.octarinecore.client.render.AbstractBlockRenderingHandler
+import mods.octarinecore.client.render.Axis
 import mods.octarinecore.client.render.BlockContext
 import mods.octarinecore.client.render.Double3
+import mods.octarinecore.client.render.Model
 import mods.octarinecore.client.render.Rotation
 import mods.octarinecore.client.render.alwaysRender
+import mods.octarinecore.client.render.cornerAo
+import mods.octarinecore.client.render.cornerFlat
+import mods.octarinecore.client.render.faceOrientedAuto
 import mods.octarinecore.client.render.modelRenderer
 import mods.octarinecore.client.render.noPost
+import mods.octarinecore.random
 import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.RenderBlocks
 import net.minecraft.init.Blocks
+import net.minecraftforge.common.util.ForgeDirection.UP
 import org.apache.logging.log4j.Level.INFO
 
 class RenderMycelium : AbstractBlockRenderingHandler(BetterFoliageMod.MOD_ID) {
 
+    // Based on the old short grass crossed square renderer
+    companion object {
+        @JvmStatic
+        val myceliumTopQuads: Model.(Int) -> Unit = { modelIdx ->
+            verticalRectangle(
+                x1 = -0.5,
+                z1 = 0.5,
+                x2 = 0.5,
+                z2 = -0.5,
+                yBottom = 0.5,
+                yTop = 0.5 + random(Config.shortGrass.heightMin, Config.shortGrass.heightMax),
+            )
+                .setAoShader(faceOrientedAuto(overrideFace = UP, corner = cornerAo(Axis.Y)))
+                .setFlatShader(faceOrientedAuto(overrideFace = UP, corner = cornerFlat))
+                .toCross(UP) { it.move(xzDisk(modelIdx) * Config.shortGrass.hOffset) }
+                .addAll()
+        }
+    }
+
     val myceliumIcon = iconSet(BetterFoliageMod.LEGACY_DOMAIN, "better_mycel_%d")
-    val myceliumModel = modelSet(64, RenderGrass.grassTopQuads)
+    val myceliumModel = modelSet(64, myceliumTopQuads)
 
     override fun afterStitch() {
         Client.log(INFO, "Registered ${myceliumIcon.num} mycelium textures")
