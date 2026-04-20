e8ff9644e5dc6cb33072a6d3153dd5cf7898c507
Kynake Risk <kynakerisk@gmail.com>
2026-04-20 02:24:37 -0300

    Delete two mycelium side textures that would've conflicted with short mycelium grass
    Add TODO comment possible fix and explanation

--- src/main/java/mods/betterfoliage/client/resource/SpriteSet.java
+++ src/main/java/mods/betterfoliage/client/resource/SpriteSet.java
@@ -20,6 +20,8 @@ public class SpriteSet extends StitchListener implements ISpriteProvider {
 
     private List<IIcon> textures;
 
+    // TODO: Consider regex instead?
+    //  Had to remove unused some mycelium textures so that they wouldn't be selected as valid
     public SpriteSet(String domain, String prefix, String suffix) {
         super();
         this.domain = domain;
