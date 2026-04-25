245d3788cb15a31560de7ec9bc967ec4c833836e
Kynake Risk <kynakerisk@gmail.com>
2026-04-25 01:12:01 -0300

    Create new reeds texture generator and renderer

---
+++ src/main/java/mods/betterfoliage/client/render/features/ReedsRenderer.java
@@ -0,0 +1,113 @@
+package mods.betterfoliage.client.render.features;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+
+import mods.betterfoliage.BetterFoliageMod;
+import mods.betterfoliage.client.config.Config;
+import mods.betterfoliage.client.render.BlockRenderer;
+import mods.betterfoliage.client.render.ISpriteProvider;
+import mods.betterfoliage.client.resource.generators.ReedsGenerator;
+import mods.betterfoliage.mixins.interfaces.minecraft.ICrossedSquaresRenderer;
+import mods.betterfoliage.utils.MathUtils;
+import mods.betterfoliage.utils.SimplexNoiseGenerator;
+import mods.octarinecore.client.render.BlockContext;
+
+public class ReedsRenderer extends BlockRenderer {
+
+    private static final int SALT = 1754;
+
+    private static ReedsRenderer instance;
+
+    private final SimplexNoiseGenerator noise = new SimplexNoiseGenerator(SALT);
+
+    private final ISpriteProvider reeds = new ReedsGenerator(
+        BetterFoliageMod.LEGACY_DOMAIN,
+        "textures/blocks/better_reed_",
+        ".png");
+
+    public static ReedsRenderer getInstance() {
+        if (instance == null) {
+            BetterFoliageMod.log.info("Registering NEW! Reeds Renderer");
+            instance = new ReedsRenderer();
+        }
+
+        return instance;
+    }
+
+    @Override
+    public boolean isEligible(BlockContext ctx) {
+        if (!Config.reed.INSTANCE.getEnabled()) return false;
+
+        Block blockAbove = ctx.block(0, 1, 0);
+        if (blockAbove.getMaterial() != Material.water) return false;
+
+        Block blockTwoAbove = ctx.block(0, 2, 0);
+        if (blockTwoAbove.getMaterial() != Material.air) return false;
+
+        if (!Config.blocks.INSTANCE.getDirt()
+            .matchesID(ctx.getBlock())) return false;
+
+        // TODO: Use [0, 1] based range
+        float threshold = Config.reed.INSTANCE.getPopulation() / 32.0f;
+        if (noise.isAboveThreshold(ctx.getX(), ctx.getZ(), threshold)) return false;
+
+        int currentBiomeId = ctx.getBiomeId();
+        for (int biomeId : Config.reed.INSTANCE.getBiomes()) {
+            if (currentBiomeId == biomeId) return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
+        RenderBlocks renderer) {
+
+        // Use original render path when rendering block breaking overlay
+        if (renderer.hasOverrideBlockTexture()) {
+            renderer.setRenderBoundsFromBlock(block);
+            return renderer.renderStandardBlock(block, x, y, z);
+        }
+
+        boolean renderResult = renderer.renderStandardBlock(block, x, y, z);
+
+        if (!renderResult) return false;
+
+        // Render Algae
+        int coordHash = MathUtils.hashCoords(x, y + 1, z, SALT);
+        double hOffset = Config.reed.INSTANCE.getHOffset();
+        double xOffset = x + MathUtils.hashToRange(MathUtils.hash(coordHash + 1), -hOffset, hOffset);
+        double zOffset = z + MathUtils.hashToRange(MathUtils.hash(coordHash + 2), -hOffset, hOffset);
+
+        float verticalScale = (float) MathUtils.hashToRange(
+            MathUtils.hash(coordHash + 3),
+            Config.reed.INSTANCE.getHeightMin(),
+            Config.reed.INSTANCE.getHeightMax());
+
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.setBrightness(Blocks.tallgrass.getMixedBrightnessForBlock(world, x, y + 2, z));
+        tessellator.setColorOpaque(0xFF, 0xFF, 0xFF);
+
+        IIcon sprite = reeds.getSpriteForCoord(x, y + 1, z);
+
+        ICrossedSquaresRenderer reedsRenderer = (ICrossedSquaresRenderer) renderer;
+        reedsRenderer.betterfoliage$setVerticalScale(verticalScale);
+
+        reedsRenderer.betterfoliage$setIsRenderingCrossedSquares(true);
+        renderer.drawCrossedSquares(
+            sprite,
+            xOffset,
+            y + 1,
+            zOffset,
+            (float) Config.algae.INSTANCE.getSize() * ReedsGenerator.HORIZONTAL_SCALING);
+        reedsRenderer.betterfoliage$setIsRenderingCrossedSquares(false);
+
+        return true;
+    }
+}
---
+++ src/main/java/mods/betterfoliage/client/resource/generators/ReedsGenerator.java
@@ -0,0 +1,109 @@
+package mods.betterfoliage.client.resource.generators;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
+
+import net.minecraft.client.resources.IResource;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
+
+import mods.betterfoliage.client.render.ISpriteProvider;
+import mods.betterfoliage.client.resource.ResourceUtils;
+import mods.betterfoliage.utils.MathUtils;
+
+public class ReedsGenerator extends TextureGenerator implements ISpriteProvider {
+
+    public static float HORIZONTAL_SCALING = 2f;
+
+    private final String baseDomain;
+
+    private final Set<ResourceLocation> backingResources;
+    private List<IIcon> sprites;
+
+    public ReedsGenerator(String domain, String prefix, String suffix) {
+        super("Generated Reeds", "Reed textures. Fitted into squares.", "gen_reeds");
+        this.baseDomain = domain;
+
+        backingResources = ResourceUtils.findResourcesWithPattern(baseDomain, prefix, suffix);
+    }
+
+    @Override
+    protected void onSpriteStitch(TextureStitchEvent.Pre event) {
+        if (event.map.getTextureType() != 0) return;
+
+        sprites = new ArrayList<>(backingResources.size());
+
+        for (ResourceLocation res : backingResources) {
+            String textureName = convertToSpriteName(res, domain);
+            if (textureName == null) continue;
+            sprites.add(event.map.registerIcon(textureName));
+        }
+    }
+
+    @Override
+    protected BufferedImage getGeneratedTexture(ResourceLocation location) throws IOException {
+        if (!resourceExists(location)) {
+            throw new IOException("Resource " + location + " is not handled by this generator!");
+        }
+
+        IResource base = ResourceUtils.getResource(remapToBase(location));
+        BufferedImage baseImage = ImageIO.read(base.getInputStream());
+
+        if (baseImage == null) {
+            throw new IIOException("Base image for generation of resource " + location + " is null!");
+        }
+
+        int baseWidth = baseImage.getWidth();
+        int xPos = baseWidth / 2;
+        int width = baseWidth * 2;
+        int frames = baseImage.getHeight() / width;
+
+        BufferedImage genImage = new BufferedImage(width, baseImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = genImage.createGraphics();
+
+        for (int frame = 0; frame < frames; frame++) {
+            BufferedImage baseSubFrame = baseImage.getSubimage(0, width * frame, baseWidth, width);
+            graphics.drawImage(baseSubFrame, xPos, width * frame, null);
+        }
+
+        return genImage;
+    }
+
+    @Override
+    protected InputStream getGeneratedMcMeta(ResourceLocation location) throws IOException {
+        return ResourceUtils.getResource(remapToBase(location))
+            .getInputStream();
+    }
+
+    @Override
+    public boolean resourceExists(ResourceLocation location) {
+        ResourceLocation remappedResource = remapToBase(location);
+        ResourceLocation remappedNonMeta = ResourceUtils.getBaseForMcMeta(remappedResource);
+
+        return backingResources.contains(remappedNonMeta)
+            && (!ResourceUtils.isMcMeta(remappedResource) || ResourceUtils.resourceHasMcMeta(remappedNonMeta));
+    }
+
+    private ResourceLocation remapToBase(ResourceLocation location) {
+        return new ResourceLocation(baseDomain, location.getResourcePath());
+    }
+
+    @Override
+    public IIcon getSpriteForCoord(int x, int y, int z) {
+        if (sprites == null || sprites.isEmpty()) {
+            return null;
+        }
+
+        int index = Math.abs(MathUtils.hashCoords(x, y, z) % sprites.size());
+        return sprites.get(index);
+    }
+}
