1463af61421fac6543429a143b0445ea97b8f4fd
Kynake Risk <kynakerisk@gmail.com>
2026-04-25 01:13:24 -0300

    Move some logic from SpriteSet into base class

--- src/main/java/mods/betterfoliage/client/resource/SpriteSet.java
+++ src/main/java/mods/betterfoliage/client/resource/SpriteSet.java
@@ -8,7 +8,6 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 
-import mods.betterfoliage.BetterFoliageMod;
 import mods.betterfoliage.client.render.ISpriteProvider;
 import mods.betterfoliage.utils.MathUtils;
 
@@ -18,7 +17,7 @@ public class SpriteSet extends StitchListener implements ISpriteProvider {
     private final String prefix;
     private final String suffix;
 
-    private List<IIcon> textures;
+    private List<IIcon> sprites;
 
     // TODO: Consider regex instead?
     // Had to remove unused some mycelium textures so that they wouldn't be selected as valid
@@ -30,7 +29,7 @@ public class SpriteSet extends StitchListener implements ISpriteProvider {
     }
 
     @Override
-    public final void onSpriteStitch(TextureStitchEvent.Pre event) {
+    protected final void onSpriteStitch(TextureStitchEvent.Pre event) {
         int type = event.map.getTextureType();
         boolean isValid = (type == 0 && prefix.startsWith("textures/blocks/"))
             || (type == 1 && prefix.startsWith("textures/items/"));
@@ -40,30 +39,22 @@ public class SpriteSet extends StitchListener implements ISpriteProvider {
         }
 
         Set<ResourceLocation> resources = ResourceUtils.findResourcesWithPattern(domain, prefix, suffix);
-        textures = new ArrayList<>(resources.size());
+        sprites = new ArrayList<>(resources.size());
 
         for (ResourceLocation res : resources) {
-            String name = res.getResourcePath();
-            int startIndex = name.lastIndexOf('/') + 1;
-            int endIndex = name.lastIndexOf('.');
-
-            if (startIndex <= 0 || startIndex > endIndex) {
-                BetterFoliageMod.log.error("Invalid resource location: {}", res);
-                continue;
-            }
-
-            String textureName = domain + ":" + name.substring(startIndex, endIndex);
-            textures.add(event.map.registerIcon(textureName));
+            String textureName = convertToSpriteName(res);
+            if (textureName == null) continue;
+            sprites.add(event.map.registerIcon(textureName));
         }
     }
 
     @Override
     public IIcon getSpriteForCoord(int x, int y, int z) {
-        if (textures == null || textures.isEmpty()) {
+        if (sprites == null || sprites.isEmpty()) {
             return null;
         }
 
-        int index = Math.abs(MathUtils.hashCoords(x, y, z) % textures.size());
-        return textures.get(index);
+        int index = Math.abs(MathUtils.hashCoords(x, y, z) % sprites.size());
+        return sprites.get(index);
     }
 }
--- src/main/java/mods/betterfoliage/client/resource/StitchListener.java
+++ src/main/java/mods/betterfoliage/client/resource/StitchListener.java
@@ -1,10 +1,12 @@
 package mods.betterfoliage.client.resource;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import mods.betterfoliage.BetterFoliageMod;
 
 public abstract class StitchListener {
 
@@ -18,6 +20,23 @@ public abstract class StitchListener {
     @SubscribeEvent
     public final void handleStitchEvent(TextureStitchEvent.Pre event) {
         onSpriteStitch(event);
+    }
+
+    protected static String convertToSpriteName(ResourceLocation location) {
+        return convertToSpriteName(location, location.getResourceDomain());
+    }
+
+    protected static String convertToSpriteName(ResourceLocation location, String domain) {
+        String name = location.getResourcePath();
+        int startIndex = name.lastIndexOf('/') + 1;
+        int endIndex = name.lastIndexOf('.');
+
+        if (startIndex <= 0 || startIndex > endIndex) {
+            BetterFoliageMod.log.error("Invalid resource location: {}", location);
+            return null;
+        }
+
+        return domain + ":" + name.substring(startIndex, endIndex);
     }
 
     protected abstract void onSpriteStitch(TextureStitchEvent.Pre event);
